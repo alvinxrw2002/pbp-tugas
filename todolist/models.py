e043a16dbbb6a9d6f6735edafa2f29f8468fd417
alvinxrw <alvin.xavier@ui.ac.id>
2022-09-28 22:06:15 +0700

    small change

--- todolist/models.py
+++ todolist/models.py
@@ -4,6 +4,6 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Task(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField(auto_now=True)
+    date = models.DateField(auto_now_add=True)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=1000000)
