73e8313007360eac316389a806bf4ab81290c669
alvinxrw <alvin.xavier@ui.ac.id>
2022-10-06 11:23:33 +0700

    textfield

--- todolist/forms.py
+++ todolist/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 class TaskForm(forms.Form):
     judul = forms.CharField(max_length=255)
-    deskripsi = forms.CharField(max_length=1000000)
+    deskripsi = forms.CharField(widget=forms.Textarea)
